0da7cddabf24a19790fc6a323a2f61b398918837
Jose Gabriel Jiménez <74217610+josegabjimenez@users.noreply.github.com>
2025-11-22 12:03:35 -0500

    Remove commented MongoDB

--- services/db.js
+++ services/db.js
@@ -9,5 +9,3 @@ class DBInstance {
 }
 
 module.exports = DBInstance;
-
-// mongoose.connect('mongodb+srv://testUser:test123456789@cluster0.ikc6z.mongodb.net/super_alitas?retryWrites=true&w=majority');
